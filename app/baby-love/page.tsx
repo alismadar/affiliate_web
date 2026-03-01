69f26a4e7d82f8fef7d541f076afa89e90788b0a
yairsmadar <yair.semama@gmail.com>
2026-03-01 22:04:35 +0200

    update baylove

--- app/baby-love/page.tsx
+++ app/baby-love/page.tsx
@@ -5,7 +5,7 @@ import Footer from "@/Components/landing/Footer";
 
 export default function BabyLovePage() {
   return (
-    <div className="min-h-screen bg-white" dir="rtl">
+    <div className="min-h-screen bg-white pb-28" dir="rtl">
       <BabyLoveHeroSection />
       <Footer />
     </div>
